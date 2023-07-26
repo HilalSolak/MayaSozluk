2f85a2a5be6d75dd2515c50d1eccaa995a7e7220
HilalSolak <56636066+HilalSolak@users.noreply.github.com>
2023-07-26 16:11:58 +0300

    Added WordRequest and WordResponse dto with record

---
+++ src/main/java/com/hilalsolak/mayasozluk/model/dto/WordRequest.java
@@ -0,0 +1,5 @@
+package com.hilalsolak.mayasozluk.model.dto;
+
+public record WordRequest(String word,String meaning,int searchCount,String createBy) {
+
+}
---
+++ src/main/java/com/hilalsolak/mayasozluk/model/dto/WordResponse.java
@@ -0,0 +1,7 @@
+package com.hilalsolak.mayasozluk.model.dto;
+
+import java.util.UUID;
+
+public record WordResponse(UUID id,String word,String meaning,int searchCount) {
+
+}
