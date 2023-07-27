9e88c1a4b19f8f383ce61bb49927c1875e8cd1d8
HilalSolak <56636066+HilalSolak@users.noreply.github.com>
2023-07-27 10:49:43 +0300

    GlobalExceptionHandler Added

---
+++ src/main/java/com/hilalsolak/mayasozluk/utils/advice/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hilalsolak.mayasozluk.utils.advice.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/hilalsolak/mayasozluk/utils/constants/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.hilalsolak.mayasozluk.utils.constants;
+
+public class GlobalConstants {
+    public static final String WORD_NOT_FOUND = " Word Not Found! ";
+    public static final String WORD_ALREADY_EXISTS = " Word Already Exists ";
+    public static final String LOGGER_NOT_FOUND = " Logger Not Found ";
+
+    private GlobalConstants(){
+
+    }
+}
