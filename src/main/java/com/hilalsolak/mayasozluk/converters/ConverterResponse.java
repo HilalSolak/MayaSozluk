2698f953cec298367558c46a78916a71aae5b5a1
HilalSolak <56636066+HilalSolak@users.noreply.github.com>
2023-07-27 10:55:54 +0300

    Added ConverterResponse class for entity-to-DTO conversion

--- src/main/java/com/hilalsolak/mayasozluk/converters/ConverterResponse.java
+++ src/main/java/com/hilalsolak/mayasozluk/converters/ConverterResponse.java
@@ -1,13 +1,20 @@
 package com.hilalsolak.mayasozluk.converters;
 
-import com.hilalsolak.mayasozluk.model.dto.WordResponse;
+import com.hilalsolak.mayasozluk.model.dto.responses.LoggerResponse;
+import com.hilalsolak.mayasozluk.model.dto.responses.WordResponse;
+import com.hilalsolak.mayasozluk.model.entities.Logger;
 import com.hilalsolak.mayasozluk.model.entities.Word;
 
 public class ConverterResponse {
 
     public static WordResponse convert(Word from){
         return new WordResponse(from.getId(),
-                from.getWord(),
+                from.getMayaWord(),
                 from.getMeaning());
     }
+    public static LoggerResponse convert(Logger from)  {
+        return new LoggerResponse(from.getId(),
+                from.getClientId(),
+                from.getActivityType(),
+                from.getCreatedTime());}
 }
